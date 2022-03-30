efcba5c985d08fc69d976b1deb0ee10e5df02734
Titu <titufrk@gmail.com>
2022-03-31 02:28:36 +0600

    Create Debug.php

---
+++ Debug.php
@@ -0,0 +1,10 @@
+<?php
+
+// Before starting theme development we have to open the debug option
+
+define( 'WP_DEBUG', false );
+
+// Change to
+
+define( 'WP_DISABLE_FATAL_ERROR_HANDLER', true ); // 5.2 and later
+define( 'WP_DEBUG', true );
